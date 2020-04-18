8a438366c09877f66ef76a90114cdf341bace404
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2020-04-18 00:13:49 -0500

    react

---
+++ LD46/Assets/Scripts/ReactorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactorController : MonoBehaviour
+{
+    public Sprite defaultState;
+    public Sprite state2;
+
+    public SpriteRenderer spriteRenderer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))// change to logic as needed
+        {
+            
+            spriteRenderer.sprite = state2;
+        }
+    }
+}
