6f0d6530b4fdfc14e1b9ce8e027143cd44f3cd0d
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2020-04-18 18:46:25 -0500

    Update DashController.cs

--- LD46/Assets/Scripts/DashController.cs
+++ LD46/Assets/Scripts/DashController.cs
@@ -280,7 +280,7 @@ public class DashController : MonoBehaviour
         if (!paused)
         {
             endHour();
-            if (UnityEngine.Random.Range(0f, 100f) >= percent)
+            if (UnityEngine.Random.Range(0f, 100f) >= percent || temp > 499 || radiation >= maxRads)
             {
                 Debug.Log("LMAO you lost");
             }
